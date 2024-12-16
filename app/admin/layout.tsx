870fe958d666fec9a66779fe839fd428df579ff1
ylh4 <yaredlema@gmail.com>
2024-12-16 15:38:37 -0500

    Update admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,6 @@
 import { AdminNavigation } from "@/components/layout/admin-navigation"
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export default function AdminLayout({
   children,
